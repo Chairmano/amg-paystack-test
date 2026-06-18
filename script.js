0607a6704cd83ce92fc7e013c7fd9a00d046bbb6
Tha Genius <126257898+Chairmano@users.noreply.github.com>
2026-06-18 12:53:18 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 function payWithPaystack() {
-    alert("Button clicked!");
+
+    let email = document.getElementById("email").value;
+
+    if (!email) {
+        alert("Please enter your email address.");
+        return;
+    }
+
+    let handler = PaystackPop.setup({
+
+        key: 'pk_test_98e526f5664747e17ffcb837e8640fe3c4526afe',
+
+        email: email,
+
+        amount: 1000, // GHS 10.00
+
+        currency: 'GHS',
+
+        callback: function(response) {
+
+            document.getElementById("result").innerHTML =
+                "Payment Successful<br><br>Reference: " +
+                response.reference;
+        },
+
+        onClose: function() {
+            alert("Payment window closed.");
+        }
+
+    });
+
+    handler.openIframe();
 }
